65150036cb3093e82a4cc240c28e6c41490fc4ee
synxless <synx@trustless.computer>
2023-03-28 16:16:46 +0700

    update

--- setup-dev.sh
+++ setup-dev.sh
@@ -13,7 +13,7 @@ sudo add-apt-repository -y ppa:luke-jr/bitcoincore
 sudo apt-get update
 sudo apt-get -y install bitcoin-qt bitcoin-tx bitcoind 
 
-bitcoin-cli -u trustless -p notrespassing stop
+bitcoin-cli -rpcuser trustless -rpcpassword notrespassing stop
 
 CONFIG_BTCD=$(cat <<EOF
 rpcuser=trustless
